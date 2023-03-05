ec6e0728d93f9f3218c41a9ab62c1b00df222806
Eren Çifci <eren.cifci04@gmail.com>
2023-03-05 12:25:49 +0300

    Fix error handler

--- src/handlers/error_handler.js
+++ src/handlers/error_handler.js
@@ -2,12 +2,12 @@ const Discord = require("discord.js");
 
 module.exports = (client) => {
     process.on("unhandledRejection", (reason, p) => {
-        console.log("Unhandled Rejection at: Promise", p);
+        console.log("Unhandled Rejection at: Promise", reason);
     });
     process.on("uncaughtException", (err, origin) => {
-        console.log("Uncaught Exception at:", origin);
+        console.log("Uncaught Exception at:", err);
     });
     process.on("uncaughtExceptionMonitor", (err, origin) => {
-        console.log("Uncaught Exception Monitor at:", origin);
+        console.log("Uncaught Exception Monitor at:", err);
     });
 };
